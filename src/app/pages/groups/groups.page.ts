88af0522d7f15c7ec8d1f2d70f906dc843cde882
tcarver371 <39978599+tcarver371@users.noreply.github.com>
2020-03-04 11:36:44 -0800

    Updated styles for the pages.

--- src/app/pages/groups/groups.page.ts
+++ src/app/pages/groups/groups.page.ts
@@ -11,8 +11,21 @@ import { CartModalPage } from '../cart-modal/cart-modal.page';
   templateUrl: './groups.page.html',
   styleUrls: ['./groups.page.scss'],
 })
-export class GroupsPage  {
-  
+export class GroupsPage implements OnInit {
+  products: Observable<any>;
 
-  constructor() { }
+  constructor(private auth: AuthService, private productService: ProductService) { }
+
+  ngOnInit() {
+    this.products = this.productService.getSellerProducts();
+  }
+
+  delete(id) {
+    this.productService.deleteProduct(id);
+  }
+
+  signOut() {
+    this.auth.signOut();
+  }
+
 }
