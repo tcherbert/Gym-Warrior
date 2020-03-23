c110dbd7c4a29f421bfe391da364141d9cea2454
Daniella Amundson <daniellaamundson@Daniellas-MBP.hitronhub.home>
2020-03-23 10:35:26 -0700

    Stil working on alertCtrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Geolocation} from '@ionic-native/geolocation/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx'
+import { AlertController } from '@ionic/angular'
 
 @Component({
   selector: 'app-root',
@@ -14,7 +16,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private oneSignal: OneSignal,
+    // private alertCtrl: AlertController
   ) {
     this.initializeApp();
   }
@@ -23,6 +27,45 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      if (this.platform.is('cordova')){
+        this.setupPush();
+      }
     });
+  }
+
+  setupPush() {
+    this.oneSignal.startInit('a8b71954-a37d-4675-81c3-cbc121d9d4fb', '79982833297');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.None);
+
+    this.oneSignal.handleNotificationOpened().subscribe(data => {
+      let additionalData = data.notification.payload.additionalData;
+      this.showAlert('Notification opened', 'You already read this', additionalData);
+    });
+
+    this.oneSignal.handleNotificationReceived().subscribe(data => {
+      let msg = data.payload.body;
+      let title = data.payload.title;
+      let additionalData = data.payload.additionalData;
+      this.showAlert(title, msg, additionalData.task);
+    });
+
+    this.oneSignal.endInit();
+  }
+
+  async showAlert(title, msg, task){
+    const alert = await this.alertCtrl.create({
+      header: title,
+      subHeader: msg, 
+      buttons: [
+        {
+          text: `Action: ${task}`,
+          handler: () => {
+
+          }     
+        }
+      ]
+    })
   }
 }
--- www/main-es2015.js
+++ www/main-es2015.js
@@ -1242,7 +1242,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/timherbert/Documents/GitHub/Gym-Warrior/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/daniellaamundson/Documents/GitHub/Gym-Warrior/src/main.ts */"./src/main.ts");
 
 
 /***/ })
